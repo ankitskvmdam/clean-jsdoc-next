019826802334fe4570be633ac9cab17f862ab2f3
Ankit Kumar <ankits.kvmdam@hotmail.com>
2023-07-06 03:38:02 +0900

    feat: handle edge case

--- frontier/components/blocks/signature.tsx
+++ frontier/components/blocks/signature.tsx
@@ -2,19 +2,30 @@
 import { ptMono } from '@/app/fonts';
 import { EKind, type TDocsDataData } from '@/types';
 import React from 'react';
+import Heading from './heading';
 
 export type TSignatureProps = {
   data: TDocsDataData;
 };
 
 export default function Signature(props: TSignatureProps) {
-  const { data } = props;
-  const { fn, attribs, returnTypes } = data.signature;
+  const {
+    data: { name, signature, kind },
+  } = props;
+
+  const _signature = {
+    fn: { name, params: [], ...signature?.fn },
+    ...signature,
+  };
+
+  const { fn, attribs, returnTypes } = _signature;
 
   return (
-    <span className={ptMono.className}>
-      {(data.kind === EKind.Class ? 'new ' : '') + fn.name}(
-      {fn.params.map((param) => param.name).join(',')})
-    </span>
+    <Heading Component="h4" className="my-4">
+      <span className={ptMono.className}>
+        {(kind === EKind.Class ? 'new ' : '') + fn.name}(
+        {fn.params.map((param) => param.name).join(',')})
+      </span>
+    </Heading>
   );
 }
