420c92f0ee4b5e1d69ea4a4dae318b6c51eb3422
Ankit Kumar <ankits.kvmdam@hotmail.com>
2023-07-06 03:38:28 +0900

    feat: add some missing sections

--- frontier/components/blocks/docs-page-data.tsx
+++ frontier/components/blocks/docs-page-data.tsx
@@ -1,6 +1,7 @@
 import type { TDocsDataAdditionalData, TDocsDataData } from '@/types';
 import Constructor from './constructor';
 import Section from './section';
+import Augments from './augments';
 
 export type TDocsPageDataProps = {
   data: TDocsDataData;
@@ -13,7 +14,15 @@ export default function DocsPageData(props: TDocsPageDataProps) {
   return (
     <div>
       <Constructor data={data} />
+      <Augments heading="Extends" id="extends" augments={data.augments} />
+      <Section data={additional.members} id="members" heading="Members" />
       <Section data={additional.methods} id="methods" heading="Methods" />
+      <Section
+        data={additional.typedefs}
+        id="typedef"
+        heading="Type Definitions"
+      />
+      <Section data={additional.events} id="events" heading="Events" />
     </div>
   );
 }
