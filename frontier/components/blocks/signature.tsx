d6c7f634348adc6df7e062862266bc112dbe257b
Ankit Kumar <ankits.kvmdam@hotmail.com>
2023-07-08 10:16:07 +0900

    feat: use old style signature

--- frontier/components/blocks/signature.tsx
+++ frontier/components/blocks/signature.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ptMono } from '@/app/fonts';
-import { EKind, type TDocsDataData } from '@/types';
+import { EKind, TParamsEntity, type TDocsDataData } from '@/types';
 import React from 'react';
 import Heading from './heading';
 
@@ -10,21 +10,15 @@ export type TSignatureProps = {
 
 export default function Signature(props: TSignatureProps) {
   const {
-    data: { name, signature, kind },
+    data: { name, signature, kind, attribs },
   } = props;
-
-  const _signature = {
-    fn: { name, params: [], ...signature?.fn },
-    ...signature,
-  };
-
-  const { fn, attribs, returnTypes } = _signature;
 
   return (
     <Heading Component="h4" className="my-4">
       <span className={ptMono.className}>
-        {(kind === EKind.Class ? 'new ' : '') + fn.name}(
-        {fn.params.map((param) => param.name).join(',')})
+        {attribs}
+        {(kind === EKind.Class ? 'new ' : '') + name}
+        {signature}
       </span>
     </Heading>
   );
